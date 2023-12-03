0f55fd215b13b92829ef8d8c61f31f579a332784
Joshua Misael Bautista Cabrera <joshmi84@hotmail.com>
2023-12-02 19:35:33 -0600

    pongo la tercera pregunta de la parte de español

--- Guaradado 1.cpp
+++ Guaradado 1.cpp
@@ -185,3 +185,91 @@ int main()
                     }
                 }while(respuesta11 !=1 && respuesta11 !=2 && respuesta11 !=3);
             }
+            if(respuesta11 ==1){
+                do{
+                    cout<<"Esta pregunta tiene un valor de 20 puntos" <<endl;
+                    cout<<"SUERTE CON LA PREGUNTA"<<endl;
+                    cout<<"¿Què es una oracion?"<<endl;
+                    cout<<"1: sentido de la expresión"<<endl;
+                    cout<<"2: Conjunto de ideas"<<endl;
+                    cout<<"3: Una unidad de comunicacion que posee sentido completo, independencia sintáctica y termina en pausa o punto"<<endl <<endl;
+                    cin>>respuesta12;
+                    
+                    switch(respuesta12){
+                        case 1:
+                            totalpuntos=totalpuntos - restapuntos;
+                            cout<<"TE EQUIVOCASTE"<<endl <<"Tu total de puntos es de: " <<totalpuntos <<" puntos"<<endl <<endl;
+                            break;
+                        case 2:
+                            totalpuntos=totalpuntos - restapuntos;
+                            cout<<"TE EQUIVOCASTE"<<endl <<"Tu total de puntos es de: " <<totalpuntos <<" puntos"<<endl <<endl;
+                            break;
+                        case 3:
+                            totalpuntos=totalpuntos + sumapuntos;
+                            cout<<"FELICIDADES"<<endl <<"Tu total de puntos es de: " <<totalpuntos<<" puntos"<<endl <<endl;
+                            break;
+                        default:
+                            cout<<"Lo lamento la opcion que eliguio no esta disponible" <<endl;
+                            cout<<"vuelve a eleguir una de las opciones anteriores"<<endl <<endl;
+                    }
+                    
+                }while(respuesta12 !=1 && respuesta12 !=2 && respuesta12 !=3);
+            }
+            if(respuesta11 ==2){
+                do{
+                    cout<<"Esta pregunta tiene un valor de 20 puntos" <<endl;
+                    cout<<"SUERTE CON LA PREGUNTA"<<endl;
+                    cout<<"¿Què es una oracion?"<<endl;
+                    cout<<"1: sentido de la expresión"<<endl;
+                    cout<<"2: Conjunto de ideas"<<endl;
+                    cout<<"3: Una unidad de comunicacion que posee sentido completo, independencia sintáctica y termina en pausa o punto"<<endl <<endl;
+                    cin>>respuesta12;
+                    
+                    switch(respuesta12){
+                        case 1:
+                            totalpuntos=totalpuntos - restapuntos;
+                            cout<<"TE EQUIVOCASTE"<<endl <<"Tu total de puntos es de: " <<totalpuntos <<" puntos"<<endl <<endl;
+                            break;
+                        case 2:
+                            totalpuntos=totalpuntos - restapuntos;
+                            cout<<"TE EQUIVOCASTE"<<endl <<"Tu total de puntos es de: " <<totalpuntos <<" puntos"<<endl <<endl;
+                            break;
+                        case 3:
+                            totalpuntos=totalpuntos + sumapuntos;
+                            cout<<"FELICIDADES"<<endl <<"Tu total de puntos es de: " <<totalpuntos<<" puntos"<<endl <<endl;
+                            break;
+                        default:
+                            cout<<"Lo lamento la opcion que eliguio no esta disponible" <<endl;
+                            cout<<"vuelve a eleguir una de las opciones anteriores"<<endl <<endl;
+                    }
+                }while(respuesta12 !=1 && respuesta12 !=2 && respuesta12 !=3);
+            }
+            if(respuesta11 ==3){
+                do{
+                    cout<<"Esta pregunta tiene un valor de 20 puntos" <<endl;
+                    cout<<"SUERTE CON LA PREGUNTA"<<endl;
+                    cout<<"¿Què es una oracion?"<<endl;
+                    cout<<"1: sentido de la expresión"<<endl;
+                    cout<<"2: Conjunto de ideas"<<endl;
+                    cout<<"3: Una unidad de comunicacion que posee sentido completo, independencia sintáctica y termina en pausa o punto"<<endl <<endl;
+                    cin>>respuesta12;
+                    
+                    switch(respuesta12){
+                        case 1:
+                            totalpuntos=totalpuntos - restapuntos;
+                            cout<<"TE EQUIVOCASTE"<<endl <<"Tu total de puntos es de: " <<totalpuntos <<" puntos"<<endl <<endl;
+                            break;
+                        case 2:
+                            totalpuntos=totalpuntos - restapuntos;
+                            cout<<"TE EQUIVOCASTE"<<endl <<"Tu total de puntos es de: " <<totalpuntos <<" puntos"<<endl <<endl;
+                            break;
+                        case 3:
+                            totalpuntos=totalpuntos + sumapuntos;
+                            cout<<"FELICIDADES"<<endl <<"Tu total de puntos es de: " <<totalpuntos<<" puntos"<<endl <<endl;
+                            break;
+                        default:
+                            cout<<"Lo lamento la opcion que eliguio no esta disponible" <<endl;
+                            cout<<"vuelve a eleguir una de las opciones anteriores"<<endl <<endl;
+                    }
+                }while(respuesta12 !=1 && respuesta12 !=2 && respuesta12 !=3);
+            }
